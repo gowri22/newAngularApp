74b95ddb13931d1c30352ee4db4482b1446c85dc
Vijaya Gowri Potturu <vpotturu@corp.paradigmit.com>
2016-11-14 17:01:10 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newAngularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the newAngularAppApp
+ */
+angular.module('newAngularAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name newAngularAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the newAngularAppApp
+ */
+angular.module('newAngularAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
